e0ebc43e46aa017b401014130537437320114ea2
Switchu94 <36130632+Switchu94@users.noreply.github.com>
2020-05-14 20:09:40 +0200

    Initial commit

---
+++ src/main/java/lr/aeris/App.java
@@ -0,0 +1,30 @@
+package lr.aeris;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
+    public static void main( String[] args )
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start (Stage primaryStage) throws Exception
+    {
+        Parent root =
+                FXMLLoader.load(
+                        getClass()
+                                .getResource("/root.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
